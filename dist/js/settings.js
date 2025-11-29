12836d2da2e72fc011e52ee12b4b47deccd4d22a
luxacious11 <starstruck_insanity@xplornet.com>
2025-11-28 23:54:33 -0400

    swap pink and blue

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -4,4 +4,4 @@ const oldSheetID = `1KDKs6Kh7dXd9V3Vgcw9ipLPiaDsUqiyRPNGYw7wFnsQ`;
 const successMessage = `<p class="fullWidth">Submission successful!</p>
 <button onclick="location.reload();" type="button" class="fullWidth submit">Back to form</button>`;
 const threadTags = ["vital", "priority", "rapidfire", "romantic", "family", "friends", "coworkers"];
-const chartColors = ['#cf86a8', '#84a9d1', '#af88ba', '#85b8b5', '#b6aa79', '#c9986e', '#84ad85', '#ba7878'];
+const chartColors = ['#84a9d1', '#cf86a8', '#af88ba', '#85b8b5', '#b6aa79', '#c9986e', '#84ad85', '#ba7878'];
