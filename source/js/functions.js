317f349624be29ee4f5e37e8ddeca2f9df050414
luxacious11 <starstruck_insanity@xplornet.com>
2026-09-12 19:43:53 -0300

    fix edit tags form

--- source/js/functions.js
+++ source/js/functions.js
@@ -101,6 +101,14 @@ function initMenus() {
             });
             document.querySelector('#loading').remove();
         }
+        //and edit tag form
+        if(document.querySelector('[data-form="edit-tags"]')) {
+            document.querySelectorAll('.accordion.sites').forEach(el => {
+                initTagSites(el, storedSites);
+                initAccordion();
+            });
+            document.querySelector('#loading').remove();
+        }
         if(document.querySelector('body.index')) {
             initIndex([...storedSites]);
         }
@@ -511,6 +519,7 @@ function initTagSelect(el, data) {
 }
 function adjustTagSites(el) {
     let existing = el.options[el.selectedIndex].dataset.sites.split(', ');
+    console.log(existing);
     el.closest('form').querySelectorAll(`.sites .multiselect label`).forEach(label => label.classList.remove('hidden'));
     existing.forEach(site => {
         if(el.closest('form').querySelector(`input[value="${site}"]`)) {
@@ -1420,8 +1429,19 @@ function updateTags(form, data) {
     let newTags = Array.from(form.querySelectorAll('.tag-options input')).map(item => item.value.toLowerCase().trim());
 
     let existing = data.filter(item => item.Tag === title)[0];
+    console.log(newSites);
     if(newSites.length > 0) {
-        let combined = [...JSON.parse(existing.Sites), ...newSites];
+        let combined;
+        //replace all
+        if(JSON.parse(existing.Sites)[0] === 'all' && newSites.length > 0 && !newSites.includes('all')) {
+            combined = [ ...newSites];
+        }
+        //switch to all
+        else if(newSites.includes('all')) {
+            combined = ['all'];
+        } else {
+            combined = [...JSON.parse(existing.Sites), ...newSites];
+        }
         existing.Sites = JSON.stringify(combined);
     }
     if(newTags.length > 0) {
@@ -1430,7 +1450,7 @@ function updateTags(form, data) {
     }
     existing.SubmissionType = 'edit-tags';
 
-    sendAjax(form, existing, successMessage);
+    //sendAjax(form, existing, successMessage);
 }
 function updatePartner(form, data) {
     let site = form.querySelector('#site').options[form.querySelector('#site').selectedIndex].innerText.trim().toLowerCase();
